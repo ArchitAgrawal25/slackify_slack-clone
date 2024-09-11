8f6c05a42eeccabb1050b6a0889f7ad4ba33ba42
Archit Agrawal <architagrawal063@gmail.com>
2024-09-11 11:42:38 +0530

    Add channel, message, and user controllers

---
+++ Backend/src/controllers/channel.controllers.js
@@ -0,0 +1,113 @@
+import { asyncHandler } from "../utils/asyncHandler.js";
+import {Channel} from "../models/channel.model.js"
+import {User} from "../models/user.model.js"
+
+
+const createChannel = asyncHandler( async (req,res) => {
+    const {channelName} = req.body;
+
+    if (!channelName) {
+        return res.status(400).json({message: "Channel Name required"})
+    }
+    if (channelName.length > 50) {
+        return res.status(400).json({message: "Channel Name should be under or below 50 characters only"})
+    }
+
+    const user = req.cookies.user ;
+
+    const channel = await Channel.create({
+        channelName,
+        createdBy: user._id,
+        users: [user._id]
+    }).catch(error => {
+        return res.status(500).json({ message: "Error creating channel", error });
+    })
+
+    await User.findByIdAndUpdate(
+        user._id,
+        { $push: {channels: channel._id}}
+    ).catch(error => {
+        return res.status(500).json({ message: "Error updating user with new channel", error });
+    });
+
+    return res
+    .status(201)
+    .json({
+        channel,
+        message: "Channel created successfully"
+    })
+
+})
+
+
+const getChannel = asyncHandler( async (req,res) => {
+    const {channelId} = req.params
+    const channel  =  await Channel.findById(channelId)
+
+    if (!channel) {
+        return res.status(404).json({message: "Channel not found"})
+    }
+    return res.status(200).json({channel, message: "Channel fetched Successfully"})
+})
+
+
+const joinChannel = asyncHandler( async (req,res) => {
+    const {userId, channelId} = req.params;
+    const user = await User.findById(userId);
+    const channel = await Channel.findById(channelId);
+
+    if (!user) {
+        return res.status(404).json({message: "user not found"})
+    }
+    if (!channel) {
+        return res.status(404).json({message: "channel not found"})
+    }
+    if(!user.channels.includes(channelId)) {
+        user.channels.push(channelId)
+        await user.save()
+    }
+    if (!channel.users.includes(userId)) {
+        channel.users.push(userId)
+        await channel.save()
+    }
+    return res.status(200).json({ message: "Joined channel successfully" });
+})
+
+
+const leaveChannel = asyncHandler( async (req,res) => {
+    const {userId, channelId} = req.params;
+    const user = await User.findById(userId);
+    const channel = await Channel.findById(channelId);
+
+    if (!user) {
+        return res.status(404).json({message: "user not found"})
+    }
+    if (!channel) {
+        return res.status(404).json({message: "channel not found"})
+    }
+
+    user.channels = user.channels.filter(id => !id.equals(channelId))
+    await user.save()
+
+    channel.users = channel.users.filter(id => !id.equals(userId))
+    await channel.save()
+
+    return res.status(200).json({ message: "Left channel successfully" });
+})
+
+
+const getChannelUsers = asyncHandler( async (req,res) => {
+    const {channelId} = req.params
+    const channel = await Channel.findById(channelId).populate('users')
+
+    if (!channel) {
+        return res.status(404).json({message: 'channel not found'})
+    }
+    return res.status(200).json({
+        users: channel.users,
+        message: "Fetched channel users successfully"
+    })
+})
+
+
+export {createChannel, getChannel, joinChannel, leaveChannel, getChannelUsers}
---
+++ Backend/src/controllers/user.controller.js
@@ -0,0 +1,114 @@
+
+import { asyncHandler } from "../utils/asyncHandler.js"
+import {User} from "../models/user.model.js"
+
+const registerUser = asyncHandler( async (req,res) => {
+    const { fullName, email, username, password} = req.body;
+    if (!fullName || !email || !username || ! password) {
+        return res.status(400).json({message: "All fields required"})
+    }
+
+    if (username.length < 5 || username.length > 20) {
+        return res.status(400).json({message: "Username should be between 5 and 20 characters"})
+    }
+
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    if (!emailRegex.test(email)) {
+        return res.status(400).json({message: "Invalid email address"})
+    }
+
+    if (password.length < 6) {
+        return res.status(400).json({message: "Password should be at least 6 characters long"})
+    } 
+
+    const existedUser = await User.findOne({
+        $or: [{ username }, { email }]
+    })
+
+    if (existedUser) {
+        return res.status(409).json({message: "User with email or username already exists"})
+    }
+
+    const user = await User.create({
+        fullName,
+        username: username.toLowerCase(),
+        email,
+        password
+    })
+    
+    const createdUser = await User.findById(user._id).select(' -password ')
+
+    if (!createdUser) {
+        return res.status(500).json({message: "Something went wrong while registering the user"})
+    }
+
+    return res.status(201).json({
+        user: createdUser,
+        message: "User register Successfully..."
+    })
+})
+
+const loginUser = asyncHandler( async (req,res) => {
+    const { username, email, password } = req.body;
+    
+    if (!(username || email)) {
+        return res.status(400).json({message: "username or email is required"})
+    }
+
+    if (!password) {
+        return res.status(400).json({message: "Password is required"})
+    }
+
+    const user = await User.findOne({
+        $or: [{ username }, { email }]
+    })
+    
+    if (!user) {
+        return res.status(404).json({message: "User does not exist"})
+    }
+
+    const isPasswordValid = await user.isPasswordCorrect(password)
+    if (!isPasswordValid) {
+        return res.status(401).json({message: "Invalid user credentials"})
+    }
+
+    const loggedInUser = await User.findById(user._id).select('-password')
+
+    res.cookie('user', loggedInUser, { httpOnly: true, secure: true });
+
+    return res
+    .status(201)
+    .json({
+        user: loggedInUser,
+        message: "User logged In Successfully..."
+    })
+})
+
+const logoutUser = asyncHandler( async (req,res) => {
+    res.clearCookie('user');
+    return res.status(200).json({ message: "User logged out successfully" });
+})
+
+const getUserProfile = asyncHandler( async (req,res) => {
+    const {username} = req.params;
+    const userProfile = await User.findOne({username}).select('-password')
+
+    if (!userProfile) {
+        return res.status(404).json({message: "User not Found"})
+    }
+
+    return res.status(200).json({ userProfile, message: "User Fetch Successfully" });
+})
+
+const getUserChannels = asyncHandler( async (req,res) => {
+    const {userId} = req.params;
+    const user = await User.findById(userId).populate('channels')
+
+    if (!user) {
+        return res.status(404).json({ message: "User not found" });
+    }
+
+    return res.status(200).json({ channels: user.channels, message: "Fetched user channels successfully" });
+})
+
+export {registerUser, loginUser, logoutUser, getUserProfile, getUserChannels}
